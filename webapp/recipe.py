7edb22227b656dd9e95780bfe06d6bb5d2eeead3
Xingyu Liu <lxy.meteor@gmail.com>
2017-03-15 21:35:20 -0400

    Build web app framework

---
+++ webapp/recipe.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook_XINGYU'
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity.
+
+    We use guestbook_name as the key.
+    """
+    return ndb.Key('Guestbook', guestbook_name)
+
+
+# [START greeting]
+class Author(ndb.Model):
+    """Sub model for representing an author."""
+    identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
+
+
+class Greeting(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    author = ndb.StructuredProperty(Author)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+# [END greeting]
+
+
+# [START main_page]
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+        greetings_query = Greeting.query(
+            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(15)
+
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'user': user,
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+# [END main_page]
+
+
+# [START Enter]
+class Enter(webapp2.RequestHandler):
+
+    def post(self):
+        flavor = self.request.get('flavor')
+        height = self.request.get('height')
+
+        self.redirect('/recipe?flavor=' + flavor + '&height=' + height)
+# [END Enter]
+
+# [START Recipe]
+class Recipe(webapp2.RequestHandler):
+
+    def get(self):
+        flavor = self.request.get('flavor')
+        height = self.request.get('height')
+
+        template_values = {
+            'flavor': flavor,
+            'height': height,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('recipe.html')
+        self.response.write(template.render(template_values))
+# [END Recipe]
+
+# [START app]
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/enter', Enter),
+    ('/recipe', Recipe),
+], debug=True)
+# [END app]
