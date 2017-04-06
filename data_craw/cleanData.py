1c3ea8f052a6b178eb56f6db39364de2f3f30d4d
WxFang <wenxinfang97@gmail.com>
2017-04-06 14:57:30 -0400

    clean

--- data_craw/cleanData.py
+++ data_craw/cleanData.py
@@ -21,13 +21,7 @@ for recipeLine in recipes:
         ingre = ingreLine[1]
         ingre = ingre.replace("_", " ")
         ingreLine[1] = ingre
-        # print ingre
-    newIngreList = []
-    for ingre in ingreList:
-        # print ingre
-        if ingre != '':
-            newIngreList.append(ingre)
-    cleanedData.append(newIngreList)
+
 
 # with open("Recipe_clean_final.csv", "wb") as f:
 #     writer = csv.writer(f)
